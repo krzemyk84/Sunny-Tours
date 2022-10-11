4020a0cbd89af4e07e75ee0bfb43c87fd5de505d
krzemyk <113865837+krzemyk84@users.noreply.github.com>
2022-10-11 21:54:20 +0000

    Added HTML basic form and adjusted JS script

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const fareAdult = 210;
 const fareChild = 110;
 
 const startProgram = () => { // function containing inputs and calculations
-  var howManyAdults = prompt ("How many adults? "); //input number of adults
-  var howManyChildren = prompt ("How many children? "); //input number of children
+
+
+
+  //var howManyAdults = prompt ("How many adults? "); //input number of adults
+  //var howManyChildren = prompt ("How many children? "); //input number of children
 
   var totalAdultHotel = howManyAdults * hotelAdultBill; //counts adults bill for the hotel  
   var totalChildrenHotel = howManyChildren * hotelChildBill; // counts children bill for the hotel
 
   var totalAdultFare = howManyAdults * fareAdult;
   var totalChildrenFare = howManyChildren * fareChild;
+  var totalTotal = totalAdultFare + totalAdultHotel + totalAdultFare + totalChildrenFare;
+
   
 }
 startProgram ();
+
+function showResults() {
+  document.getElementById("results").innerHTML = "total";
+}
+
